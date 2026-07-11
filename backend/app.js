c7823a85544165193bbf0223989dc703da8a6c4e
Hasnain <hasnainqurban284@gmail.com>
2026-07-11 07:11:32 -0700

    Fix CORS origin URL

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const apiLimiter = rateLimit({
 
 // Middlewares
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "https://hasnain-mern-10pshine.onrender.comhttp://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 }));
 app.use(express.json());
