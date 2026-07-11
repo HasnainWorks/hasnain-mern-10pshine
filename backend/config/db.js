e301f891c89d10e20e14af0ffca23eb29a937c2d
Hasnain <hasnainqurban284@gmail.com>
2026-07-11 06:59:27 -0700

    Fix DB connection error and update Mongo URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,9 @@ const logger = require("../utils/logger");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     logger.info("MongoDB Atlas Connected");
   } catch (error) {
-    logger.error("DB Connection Error:", error.message);
+    logger.error({ err: error }, "DB Connection Error");
     process.exit(1);
   }
 };
